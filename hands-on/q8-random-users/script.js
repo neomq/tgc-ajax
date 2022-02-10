e1d9e778134591dc247cee651af495db3ad06033
neomq <neominqi@hotmail.com>
2022-02-10 17:53:10 +0000

    done qn 8

--- hands-on/q8-random-users/script.js
+++ hands-on/q8-random-users/script.js
@@ -1,1 +1,23 @@
-let url = "https://randomuser.me/api/?results=10"
+
+let url = "https://randomuser.me/api/?results=10";
+
+
+document.querySelector("#btn").addEventListener('click', async function(){
+
+    await axios.get(url).then(function(response){
+        let randUsers = response.data
+        let randUsersList = document.querySelector('#display');
+
+        for (let user of randUsers.results){
+            let userName = user.name.first + " " + user.name.last;
+            let userCity = user.location.city;
+
+            randUsersList.innerHTML += `<p>Name: ${userName}<br>City: ${userCity}</p>`;
+
+            let userImg = user.picture.medium;
+            randUsersList.innerHTML += `<img src="${userImg}" alt="">`;
+
+        }
+    })
+
+})
