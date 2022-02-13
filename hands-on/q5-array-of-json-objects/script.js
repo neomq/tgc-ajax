0ef6b137f880bb6e1a244309b1ac2ed2717ad6ee
Min Qi Neo <neominqi@hotmail.com>
2022-02-14 01:27:14 +0800

    updated q5

--- hands-on/q5-array-of-json-objects/script.js
+++ hands-on/q5-array-of-json-objects/script.js
@@ -1,4 +1,4 @@
-document.querySelector('#load-users-btn').addEventListener('click', async function(){
+document.querySelector('#load-users').addEventListener('click', async function(){
         let response = await axios.get('users.json');
         let userData = response.data;
         
@@ -6,15 +6,32 @@ document.querySelector('#load-users-btn').addEventListener('click', async functi
         for(let user of userData.users){
             //console.log(user);
             
-            let userInfo = document.createElement('li');
             let userFirstName = user.firstName;
             let userLastName = user.lastName;
             let userEmail = user.emailAddress;
 
-            userInfo.innerText = "First Name: " + userFirstName + "\n" + "Last Name: " + userLastName + "\n" + "Email: " + userEmail;
-            document.querySelector('#users').appendChild(userInfo);
+            let userInfo = document.createElement('li');
+            let displayName = document.createElement('div');
+            let displayEmail = document.createElement('small');
 
-            
+            displayName.innerHTML = userFirstName + " " + userLastName;
+            displayEmail.innerHTML = userEmail;
 
+            userInfo.appendChild(displayName);
+            userInfo.appendChild(displayEmail); 
+            document.querySelector('#all-users-list').appendChild(userInfo);
+
+            // bootstrap
+            userInfo.className = "list-group-item justify-content-between align-items-start";
+            displayName.className = "fw-bold";
         }
+});
+
+document.querySelector('#search-btn').addEventListener('click', async function(){
+        let response = await axios.get('users.json');
+        let userData = response.data;
+
+        let searchInput = document.querySelector('#search-input').value;
+        
+        ///// Incomplete /////
 });
